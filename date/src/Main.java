86b7c8a1243c3940e81e8b65eb883c34c5ba824b
Mahdie Montazeralzohoor <mmontazer1831@gmail.com>
2021-09-24 21:02:36 +0330

    add the last two methods

--- date/src/Main.java
+++ date/src/Main.java
@@ -35,6 +35,7 @@ public class Main {
             System.out.println("Invalid input. ");
         }
 
+
     }
 
 
--- date/src/MyDate.java
+++ date/src/MyDate.java
@@ -1,5 +1,31 @@
 public class MyDate {
 
+    private int day, month, year;
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
     public boolean isValidDate(int year, int month, int day) {
 
         if (year < 9999 && year >= 1) {
@@ -30,4 +56,32 @@ public class MyDate {
         }
         return null;
     }
+
+    public void nextDay() {
+        if (day == getMonthLastDay(month)) {
+            if (month == 12) {
+                setDay(1);
+                setMonth(1);
+                setYear(year + 1);
+            } else {
+                setDay(1);
+                setMonth(month + 1);
+            }
+
+        } else {
+            setDay(day + 1);
+        }
+
+    }
+
+    public String toString() {
+        String day = Integer.toString(this.day);
+        String year = Integer.toString(this.year);
+        MonthName month = getMonthName(this.month);
+
+
+        return day + " " + month + " " + year;
+
+    }
+
 }
